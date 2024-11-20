023721222654b6b86db603ec3e70648e4a0289e1
yabibal <eshetieyabibal@gmail.com>
2024-11-20 04:02:04 -0500

    add pages and components

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  About = () => {
+  return (
+    <div className='flex mx-auto justify-center h-screen items-center text-8xl'> About Page </div>
+  )
+}
+
+export default  About 
---
+++ src/pages/Enterprise.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Enterprise = () => {
+  return (
+    <div className='flex mx-auto justify-center h-screen items-center text-8xl'> Enterprise Page </div>
+  )
+}
+
+export default Enterprise
